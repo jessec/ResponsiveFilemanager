0f2b88fddb7693e8e4785af5b9c63c56e9c6df74
jessec <robertjessecooke@gmail.com>
2014-11-01 19:47:01 +0100

    cc

--- init.js
+++ init.js
@@ -11,19 +11,41 @@ function loadCss(url){
 	link.setAttribute("href",url);
 	document.getElementsByTagName("head")[0].appendChild(link);
 }
-loadCss("assets/bootstrap.min.css");
-loadCss("assets/bootstrap-responsive.min.css");
-loadCss("assets/animate.min.css");
-loadCss("assets/style.css");
-loadCss("assets/style.css");
-loadCss("assets/jquery.fancybox-1.3.4.css");
-loadCss("assets/font-awesome.min.css");
-loadCss("assets/prettyPhoto.css");
+function loadCssUrls(baseUrl){
+loadCss(baseUrl + "assets/bootstrap.min.css");
+loadCss(baseUrl + "assets/bootstrap-responsive.min.css");
+loadCss(baseUrl + "assets/animate.min.css");
+loadCss(baseUrl + "assets/style.css");
+loadCss(baseUrl + "assets/style.css");
+loadCss(baseUrl + "assets/jquery.fancybox-1.3.4.css");
+loadCss(baseUrl + "assets/font-awesome.min.css");
+loadCss(baseUrl + "assets/prettyPhoto.css");
+}
+// Handles message from ResponsiveFilemanager
+//
+function OnMessage(e) {
+	var event = e.originalEvent;
+	// Make sure the sender of the event is trusted
+	if (event.data.sender === 'responsivefilemanager') {
+		if (event.data.field_id) {
+			var fieldID = event.data.field_id;
+			var url = event.data.url;
+			$('#' + fieldID).val(url).trigger('change');
+			$.fancybox.close();
 
+			// Delete handler of the message from ResponsiveFilemanager
+			$(window).off('message', OnMessage);
+		}
+	}
+}
 
-function  initMyPage(){
+function  initMyPage(baseUrl){
+
+	loadCssUrls(baseUrl);
 
 jQuery(document).ready(function($) {
+
+
 			$('.iframe-btn').fancybox({
 				'width' : 880,
 				'height' : 570,
@@ -31,31 +53,17 @@ jQuery(document).ready(function($) {
 				'autoScale' : false
 			});
 
-			// Handles message from ResponsiveFilemanager
-			//
-			function OnMessage(e) {
-				var event = e.originalEvent;
-				// Make sure the sender of the event is trusted
-				if (event.data.sender === 'responsivefilemanager') {
-					if (event.data.field_id) {
-						var fieldID = event.data.field_id;
-						var url = event.data.url;
-						$('#' + fieldID).val(url).trigger('change');
-						$.fancybox.close();
 
-						// Delete handler of the message from ResponsiveFilemanager
-						$(window).off('message', OnMessage);
-					}
-				}
-			}
 
 			// Handler for a message from ResponsiveFilemanager
-			$('.iframe-btn').on('click', function() {
+			$('.iframe-btn').on('click', function(e) {
+				e.preventDefault();
 				$(window).on('message', OnMessage);
+
 			});
 
 		});
 
 }
 
-initMyPage();
+initMyPage(baseUrl);
